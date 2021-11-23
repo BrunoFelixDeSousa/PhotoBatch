d6130b99698d5aa4a2dbfe4bda70061aca1b70d0
Bruno Felix de Sousa <brunofelixdesousa@hotmail.com>
2021-11-22 22:55:49 -0300

    atualizando PhotoBatch

--- PhotoBatch.cpp
+++ PhotoBatch.cpp
@@ -1,13 +1,60 @@
 #include <iostream>
+#include <iomanip>
+#include <map>
+
+class ArgumentParser
+{
+public:
+	void RegisterFlag(const std::string& flag)
+	{
+		if (!flag.empty())
+		{
+			m_Flags[flag] = false;
+		}
+	}
+
+	bool GetFlag(const std::string& flag)
+	{
+
+		if (!flag.empty())
+		{
+			auto flagIt = m_Flags.find(flag);
+			if (flagIt != std::(end(m_Flags)
+			{
+				return flagIt->second;
+			}
+			
+		}
+		return false;
+	}
+
+	void Parse(int argc, char* argv[])
+	{
+
+	}
+
+private:
+	std::map<std::string, bool> m_Flags;
+};
 
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "pt_BR");
 	
-	//std::cout << "Bruno Félix" << std::endl;
-	for ( int i=0; i < argc; i++ ) {
-		std::cout << argc << " " << argv[i] << '\n';
-	}
+	ArgumentParser argParser;
+
+	argParser.RegisterFlag("rename");
+	argParser.RegisterFlag("convert");
+	argParser.RegisterFlag("rezise");
+	argParser.RegisterFlag("scale");
+
+	argParser.Parse(argc, argv);
+
+	std::cout << std::boolalpha << "Rename  : " << argParser.GetFlag("rename") << '\n';
+	std::cout << std::boolalpha << "Convert : " << argParser.GetFlag("convert") << '\n';
+	std::cout << std::boolalpha << "Rezise  : " << argParser.GetFlag("rezise") << '\n';
+	std::cout << std::boolalpha << "Scale   : " << argParser.GetFlag("scale") << '\n';
+
 
 	return 0;
 }
